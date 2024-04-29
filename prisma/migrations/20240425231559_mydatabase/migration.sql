cdc208e67e400a1fcc95be2c4b50f9477e62c494
MatiasC5 <cardosomatias1994@gmail.com>
2024-04-29 13:56:04 -0300

    asdasdsa

--- prisma/migrations/20240425231559_mydatabase/migration.sql
+++ prisma/migrations/20240425231559_mydatabase/migration.sql
@@ -1,12 +1,12 @@
 -- CreateTable
-CREATE TABLE "user" (
+CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
 
-    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
  
